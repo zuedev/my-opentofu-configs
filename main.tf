de91cc612fbd256281fc596364e1c14e28e5d773
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-01-15 13:55:14 +0000

    Add Terraform configuration for AWS S3 backend and resources

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket       = "zuedev-opentofu-state"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
